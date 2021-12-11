de2daaec0022c724c8e1717b7b90230d7270ff95
Pawel Szwagierek <szwagier90@gmail.com>
2021-12-11 15:06:37 +0000

    woring

--- c/main.cpp
+++ c/main.cpp
@@ -9,56 +9,39 @@ std::pair<std::list<std::list<int>> ,bool> find_subsets(
     int target, 
     std::list<int> & partial)
 {
-    std::cout << "it_start val: " << *it_start << std::endl;
-    std::cout << "it_start: " << &*it_start << std::endl;
-    std::cout << "it_end: " << &*it_end << std::endl;
-
-    for (; it_start != it_end ; ++it_start)
-    std::cout << std::endl << "Partial: ";
-    for(const auto & i : partial)
-    {
-        std::cout << i << " ";
-    }
-    std::cout << std::endl;
     std::list<std::list<int>> partial_subsets;
 
-    int sum = std::accumulate(partial.begin(), partial.end(), 0);
-    std::cout << "Sum: " << sum << std::endl;
-
-    if(sum == target)
+    for (; it_start != it_end ; ++it_start)
     {
-        partial_subsets.push_back(std::list<int>(partial));
-    }
-    if (sum >= target)
-    {
-        return std::make_pair(partial_subsets, true);
-    }
-
-    std::vector<int>::iterator remaining;
-    {
-        std::cout << "It: " << *it << std::endl;
-        std::cout << "remaining: " << *remaining << std::endl;
-        auto pin = *remaining++;
-        if (remaining == data.end())
+        partial.push_back(*it_start);
+/*
+        std::cout << "Partial: ";
+        for(const auto & i : partial)
         {
-            std::cout << "remaining == data.end()" << std::endl;
-            break;
+            std::cout << i << " ";
         }
-        std::cout << "inc" << std::endl;
-        std::cout << "It: " << *it << std::endl;
-        std::cout << "remaining: " << *remaining << std::endl;
-        std::cout << "Inserting to partial: " << pin << std::endl;
-        partial.push_back(pin);
-        auto found = find_subsets(data, remaining, target, partial);
+        std::cout << std::endl;
+*/
+        int sum = std::accumulate(partial.begin(), partial.end(), 0);
+        if(sum == target)
+        {
+            partial_subsets.push_back(std::list<int>(partial));
+        }
+        if (sum >= target)
+        {
+            int popped = partial.back();
+            partial.pop_back();
+            return std::make_pair(partial_subsets, true);
+        }
+
+        std::vector<int>::iterator remaining = it_start;
+        
+        remaining++;
+        auto found = find_subsets(remaining, it_end, target, partial);
         partial_subsets.insert(partial_subsets.end(), found.first.begin(), found.first.end());
-        if (found.second == true)
-        {
-            break;
-        }
+        int popped = partial.back();
+        partial.pop_back();
     }
-    int popped = partial.back();
-    partial.pop_back();
-    std::cout << "Popped from partial: " << popped << std::endl;
 
     return std::make_pair(partial_subsets, false);
 }
@@ -66,13 +49,13 @@ std::pair<std::list<std::list<int>> ,bool> find_subsets(
 
 int main()
 {
-    // std::vector<int> v = {2016, 2016, 2016, 2016, 2856, 2957, 5914, 48579, 48579, 5292, 6174, 65541, 69548, 7056, 71551, 71551, 124053, 253422, 270116, 357669, 365485, 375056, 1914946, 2351322, 2875653, 3115457, 3201977};
-    std::vector<int> v = {1, 2, 4, 8};
+    // std::vector<int> v = {2016, 2016, 2016, 2016, 2856, 2957, 5914, 48579, 48579, 529200, 617400, 65541, 69548, 705600, 71551, 71551, 124053, 253422, 270116, 357669, 365485, 375056, 1914946, 2351322, 2875653, 3115457, 3201977};
+    std::vector<int> v = {100, 105, 206, 309, 555, 763, 829, 1052, 2016, 2016, 2016, 2016, 2375, 2856, 2957, 3461, 4091, 5914, 738, 10000, 20000, 30000, 48579, 48579, 52920, 61740, 65541, 69548, 70560, 71551, 71551, 124053, 253422, 270116, 357669, 365485, 375056, 1914946, 2351322, 2875653, 3115457, 3201977};
 
     std::list<int> partial;
 
     std::pair<std::list<std::list<int>>, bool> all_subsets = 
-        find_subsets(v.begin(), v.end(), 6, partial);
+        find_subsets(v.begin(), v.end(), 56509, partial);
 
     for(const auto & subset : all_subsets.first)
     {
